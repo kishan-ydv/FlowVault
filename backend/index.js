7bd1621e889e3ab6f87382dfc5db0dbd77c14515
kishan yadav <kshn50967@gmail.com>
2025-06-12 18:41:05 +0530

    another commit

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const rootRouter = require("./routes/index");
 const cors = require('cors')
 const app = express();
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json())
 app.use("/api/v1",rootRouter);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
