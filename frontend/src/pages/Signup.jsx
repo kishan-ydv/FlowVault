0d32ef1a84f014e23d2896ed322924c9d28bf609
Kishan Yadav <kshn50967@gmail.com>
2025-06-14 04:45:15 +0530

    Commit

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -44,6 +44,9 @@ export const Signup = () => {
 
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="text-xl font-bold">
+                FlowVault
+            </div>
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign up"} />
